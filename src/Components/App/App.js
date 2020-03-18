1c7339cad0ee57949497af0ebd4ec5cad7329b26
Rafael Montas <rafaelmontas1@gmail.com>
2020-03-18 13:32:23 -0400

    Add Tracks to a Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -25,6 +25,16 @@ class App extends React.Component {
       playlistName: "My personal playlist",
       playlistTracks: [{id: 4, name: "playlistName1", artist: "playlistArtist1", album: "playlistAlbum1"}]
     }
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
   }
   
   render() {
@@ -34,7 +44,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar/>
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
